551df61238b0724f0b1dfd7c5dafc6166a93e567
HunorMarton <bor.hunor@gmail.com>
2020-08-19 20:44:49 +0200

    Surrounding bombs

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,21 @@
 export const totalNumberOfBombs = 14;
 
+export function getIndex(row, column) {
+  if (row < 0) return;
+  if (column < 0) return;
+  if (row > 9) return;
+  if (column > 9) return;
+
+  return row * 10 + column;
+}
+
+export function getCoordinates(index) {
+  return {
+    row: Math.floor(index / 10),
+    column: index % 10,
+  };
+}
+
 export function generateTiles() {
   const bombs = Array.from({ length: 100 });
 
@@ -13,5 +29,19 @@ export function generateTiles() {
     }
   }
 
-  return bombs;
+  return bombs.map((bomb, index, array) => {
+    const { row, column } = getCoordinates(index);
+
+    let surroundingBombs = 0;
+    if (array[getIndex(row - 1, column - 1)]) surroundingBombs++;
+    if (array[getIndex(row - 1, column - 0)]) surroundingBombs++;
+    if (array[getIndex(row - 1, column + 1)]) surroundingBombs++;
+    if (array[getIndex(row - 0, column - 1)]) surroundingBombs++;
+    if (array[getIndex(row - 0, column + 1)]) surroundingBombs++;
+    if (array[getIndex(row + 1, column - 1)]) surroundingBombs++;
+    if (array[getIndex(row + 1, column - 0)]) surroundingBombs++;
+    if (array[getIndex(row + 1, column + 1)]) surroundingBombs++;
+
+    return { bomb, surroundingBombs };
+  });
 }
